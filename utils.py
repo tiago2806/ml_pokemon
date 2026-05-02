a2b2d8256eb4fe4856e098dc46b7e8b3d4542d34
Tiago <tiagohjcarvalho@gmail.com>
2026-05-02 16:16:00 +0100

    clustering original features

---
+++ utils.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram
+
+
+def plot_dendrogram(model, **kwargs):
+    '''
+    Create linkage matrix and then plot the dendrogram
+    Arguments: 
+    - model(HierarchicalClustering Model): hierarchical clustering model.
+    - **kwargs
+    Returns:
+    None, but dendrogram plot is produced.
+    '''
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack(
+        [model.children_, model.distances_, counts]
+    ).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
